a768539d90036cd9fc3394d759003115505e2b76
juandesousa <juan.cdsb@gmail.com>
2021-10-22 11:45:23 +0200

    homework

--- server.js
+++ server.js
@@ -4,22 +4,107 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
+const messages = [
+  { id: 0, from: "Bart", text: "Welcome to CYF chat system!" }
+];
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+const newId = () => {
+  const lastMessageIndex = messages.length - 1;
+  if (lastMessageIndex === -1) {
+    return 0;
+  } else {
+    const nextId = lastMessageIndex + 1;
+    return nextId;
+  }
+}
 
-app.get("/", function (request, response) {
+const completeMessage = (message) => {
+  if (message.text && message.from) {
+    return true;
+  }
+  return false;
+}
+
+app.get("/",  (request, response) => {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, () => {
-   console.log("Listening on port 3000")
-  });
+app.get("/messages", (req, res) => {
+  res.status(201).send(messages);
+});
+
+
+app.get("/messages/search", (req, res) => {
+  const searchTerm = req.query.text.toLowerCase()
+  const result = messages.filter(item => item.text.toLowerCase().includes(searchTerm))
+  res.send(result)
+})
+
+app.get("/messages/latest", (req, res) => {
+  let result = messages.slice(-10)
+  res.send(result)
+});
+
+
+app.get("/messages/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+  const message = messages.find((message) => message.id === messageId);
+
+  if (message) {
+    res.status(201).send(message);
+  } else {
+    res.status(404).send("This message does not exist");
+  }
+});
+
+
+
+app.post("/messages", (req, res) => {
+  const message = {
+    id:  newId(),
+    from: req.body.from,
+    text: req.body.text,
+  };
+
+  message.timeSent = new Date()
+
+  if (!completeMessage(message)) {
+    res.status(404).send("This message is not complete.");
+    return;
+  }
+  messages.push(message);
+  res.status(201).send(message);
+});
+
+app.put("/messages/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+  let updatedMessage = req.body;
+
+  let message = messages.find((message) => message.id === messageId);
+  if (!message) {
+    res.status(404).send("This message does not exist");
+  }
+    message.from = updatedMessage.from;
+    message.text = updatedMessage.text;
+    timeSent = message.timeSent
+    res.status(201).send(updatedMessage);
+});
+
+app.delete("/messages/:id", (req, res) => {
+  const messageId = req.params.id;
+
+  const index = messages.findIndex((message) => message.id == messageId);
+  if (index === -1) {
+    res.status(404).send();
+    return;
+  }
+
+  messages.splice(index, 1);
+  res.status(201).send({ success: true });
+});
+
+app.listen(3001, () => {
+  console.log("Listening on port 3001");
+});
